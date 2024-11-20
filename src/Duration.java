11fc2bad6dfc706a2050d6918666ab443def26db
YK <jqt24wgu@uea.ac.uk>
2024-11-20 15:44:32 +0000

    refactor Duration class

--- src/Duration.java
+++ src/Duration.java
@@ -1,219 +1,171 @@
 import java.util.ArrayList;
 
 public class Duration implements Comparable<Duration> {
-  private int hours;
-  private int minutes;
-  private int seconds;
+  private int _hours;
+  private int _minutes;
+  private int _seconds;
 
-  public Duration() {
-    this(0, 0, 0);
+  public Duration(int seconds) {
+    _hours = 0;
+    _minutes = 0;
+    _seconds = Math.max(0, seconds);
+    normalise();
   }
 
   public Duration(int hours, int minutes, int seconds) {
-    setHours(hours);
-    setMinutes(minutes);
-    setSeconds(seconds);
+    _hours = Math.max(0, hours);
+    _minutes = Math.max(0, minutes);
+    _seconds = Math.max(0, seconds);
+    normalise();
   }
 
+  public int getHours() {
+    return _hours;
+  }
+
+  public int getMinutes() {
+    return _minutes;
+  }
+
+  public int getSeconds() {
+    return _seconds;
+  }
+
+  @Override
   public String toString() {
-    return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    return String.format("%02d:%02d:%02d", _hours, _minutes, _seconds);
   }
 
-
-  /* converting the duration of the songs to the second for easier compare */
-  private int totalSeconds() {
-    return (hours * 3600) +(minutes * 60) + seconds;
-  }
- /* comparing one duration to another */
+  @Override
   public int compareTo(Duration other) {
     return Integer.compare(totalSeconds(), other.totalSeconds());
   }
 
-  /* time setters */
-  public void setSeconds(int seconds) {
-    this.seconds = Math.clamp(seconds, 0, 59); 
+  private void normalise() {
+    _minutes += _seconds / 60;
+    _seconds %= 60;
+
+    _hours += _minutes / 60;
+    _minutes %= 60;
   }
 
-  public void setMinutes(int minutes) {
-    this.minutes = Math.clamp(minutes, 0, 59);
-  }
-
-  public void setHours(int hours) {
-    this.hours = Math.clamp(hours, 0, Integer.MAX_VALUE);
-  }
-
-  /* getters */
-  public int getSeconds() {
-    return seconds;
-  }
-
-  public int getMinutes() {
-    return minutes;
-  }
-
-  public int getHours() {
-    return hours;
+  private int totalSeconds() {
+    return _hours * 3600 + _minutes * 60 + _seconds;
   }
 
   public static void main(String[] args) {
     var passed = true;
 
-    if (!testConstructors()) passed = false;
-    if (!testGetSet()) passed = false;
+    if (!testConstructorsAndGets()) passed = false;
     if (!testCompareTo()) passed = false;
 
     if (!passed) System.err.println("Error: Test Duration::main failed");
   }
 
-  private static boolean testConstructors() {
+  private static boolean testConstructorsAndGets() {
     var passed = true;
 
-    var d = new Duration();
-    if (d.hours != 0 || d.minutes != 0 || d.seconds != 0) {
-      System.err.printf(
-          "Error: Test Duration::testConstructors failed: new Duration() is Duration{%d, %d, %d}",
-          d.hours, d.minutes, d.seconds);
-      passed = false;
-    }
+    var constructorArguments = new ArrayList<int[]>();
+    var expectedComponents = new ArrayList<int[]>();
 
-    d = new Duration(0, 0, 0);
-    if (d.hours != 0 || d.minutes != 0 || d.seconds != 0) {
-      System.err.printf(
-          "Error: Test Duration::testConstructors failed: new Duration(0, 0, 0) is Duration{%d, %d, %d}",
-          d.hours, d.minutes, d.seconds);
-      passed = false;
-    }
+    constructorArguments.add(new int[]{-1});
+    expectedComponents.add(new int[]{0, 0, 0});
+    
+    constructorArguments.add(new int[]{0});
+    expectedComponents.add(new int[]{0, 0, 0});
 
-    d = new Duration(1, 2, 3);
-    if (d.hours != 1 || d.minutes != 2 || d.seconds != 3) {
-      System.err.printf(
-          "Error: Test Duration::testConstructors failed: new Duration(1, 2, 3) is Duration{%d, %d, %d}",
-          d.hours, d.minutes, d.seconds);
-      passed = false;
-    }
+    constructorArguments.add(new int[]{1});
+    expectedComponents.add(new int[]{0, 0, 1});
 
-    d = new Duration(-1, -2, -3);
-    if (d.hours != 0 || d.minutes != 0 || d.seconds != 0) {
-      System.err.printf(
-          "Error: Test Duration::testConstructors failed: new Duration(-1, -2, -3) is Duration{%d, %d, %d}",
-          d.hours, d.minutes, d.seconds);
-      passed = false;
-    }
+    constructorArguments.add(new int[]{60});
+    expectedComponents.add(new int[]{0, 1, 0});
 
-    d = new Duration(100, 200, 300);
-    if (d.hours != 100 || d.minutes != 59 || d.seconds != 59) {
-      System.err.printf(
-          "Error: Test Duration::testConstructors failed: new Duration(100, 200, 300) is Duration{%d, %d, %d}",
-          d.hours, d.minutes, d.seconds);
-      passed = false;
-    }
+    constructorArguments.add(new int[]{3600});
+    expectedComponents.add(new int[]{1, 0, 0});
 
-    return passed;
-  }
+    constructorArguments.add(new int[]{3661});
+    expectedComponents.add(new int[]{1, 1, 1});
 
-  private static boolean testGetSet() {
-    var passed = true;
+    constructorArguments.add(new int[]{-1, -1, -1});
+    expectedComponents.add(new int[]{0, 0, 0});
 
-    var d = new Duration();
+    constructorArguments.add(new int[]{0, 0, 0});
+    expectedComponents.add(new int[]{0, 0, 0});
 
-    for (var i = 0; i < 60; ++i) {
-      d.setHours(i);
-      var result = d.getHours();
-      if (result != i) {
+    constructorArguments.add(new int[]{1, 2, 3});
+    expectedComponents.add(new int[]{1, 2, 3});
+
+    constructorArguments.add(new int[]{60, 60, 60});
+    expectedComponents.add(new int[]{61, 1, 0});
+
+    constructorArguments.add(new int[]{100, 200, 300});
+    expectedComponents.add(new int[]{103, 25, 0});
+
+    for (var i = 0; i < constructorArguments.size(); ++i) {
+      var arguments = constructorArguments.get(i);
+
+      Duration d;
+      String allocationString;
+      if (arguments.length == 1) {
+        d = new Duration(arguments[0]);
+        allocationString = String.format("new Duration(%d)", arguments[0]);
+      } else {
+        d = new Duration(arguments[0], arguments[1], arguments[2]);
+        allocationString = String.format("new Duration(%d, %d, %d)", arguments[0], arguments[1], arguments[2]);
+      }
+
+      var expectedHours = expectedComponents.get(i)[0];
+      var expectedMinutes = expectedComponents.get(i)[1];
+      var expectedSeconds = expectedComponents.get(i)[2];
+
+      if (d._hours != expectedHours || d._minutes != expectedMinutes || d._seconds != expectedSeconds) {
         System.err.printf(
-            "Error: Test Duration::testGetSet failed: duration.setHours(%d); duration.getHours() == %d\n",
-            i, result);
+            "Error: Test Duration::testConstructorsAndGets failed: %s is Duration{%d, %d, %d}\n",
+            allocationString, d._hours, d._minutes, d._seconds);
         passed = false;
       }
 
-      d.setMinutes(i);
-      result = d.getMinutes();
-      if (result != i) {
+      var hours = d.getHours();
+      if (hours != expectedHours) {
         System.err.printf(
-            "Error: Test Duration::testGetSet failed: duration.setMinutes(%d); duration.getMinutes() == %d\n",
-            i, result);
+            "Error: Test Duration::testConstructorsAndGets failed: Duration{%d, %d, %d}.getHours() == %d\n",
+            d._hours, d._minutes, d._seconds, hours);
         passed = false;
       }
 
-      d.setSeconds(i);
-      result = d.getSeconds();
-      if (result != i) {
+      var minutes = d.getMinutes();
+      if (minutes != expectedMinutes) {
         System.err.printf(
-            "Error: Test Duration::testGetSet failed: duration.setSeconds(%d); duration.getSeconds() == %d\n",
-            i, result);
+            "Error: Test Duration::testConstructorsAndGets failed: Duration{%d, %d, %d}.getMinutes() == %d\n",
+            d._hours, d._minutes, d._seconds, minutes);
+        passed = false;
+      }
+
+      var seconds = d.getSeconds();
+      if (seconds != expectedSeconds) {
+        System.err.printf(
+            "Error: Test Duration::testConstructorsAndGets failed: Duration{%d, %d, %d}.getSeconds() == %d\n",
+            d._hours, d._minutes, d._seconds, seconds);
         passed = false;
       }
     }
 
-    d.setHours(-1);
-    var result = d.getHours();
-    if (result != 0) {
-      System.err.printf(
-          "Error: Test Duration::testGetSet failed: duration.setHours(%d); duration.getHours() == %d\n",
-          -1, result);
-      passed = false;
-    }
-
-    d.setMinutes(-2);
-    result = d.getMinutes();
-    if (result != 0) {
-      System.err.printf(
-          "Error: Test Duration::testGetSet failed: duration.setMinutes(%d); duration.getMinutes() == %d\n",
-          -2, result);
-      passed = false;
-    }
-
-    d.setSeconds(-3);
-    result = d.getSeconds();
-    if (result != 0) {
-      System.err.printf(
-          "Error: Test Duration::testGetSet failed: duration.setSeconds(%d); duration.getSeconds() == %d\n",
-          -3, result);
-      passed = false;
-    }
-
-    d.setHours(100);
-    result = d.getHours();
-    if (result != 100) {
-      System.err.printf(
-          "Error: Test Duration::testGetSet failed: duration.setHours(%d); duration.getHours() == %d\n",
-          100, result);
-      passed = false;
-    }
-
-    d.setMinutes(200);
-    result = d.getMinutes();
-    if (result != 59) {
-      System.err.printf(
-          "Error: Test Duration::testGetSet failed: duration.setMinutes(%d); duration.getMinutes() == %d\n",
-          200, result);
-      passed = false;
-    }
-
-    d.setSeconds(300);
-    result = d.getSeconds();
-    if (result != 59) {
-      System.err.printf(
-          "Error: Test Duration::testGetSet failed: duration.setSeconds(%d); duration.getSeconds() == %d\n",
-          300, result);
-      passed = false;
-    }
-
-    return passed;
+    return  passed;
   }
 
   private static boolean testCompareTo() {
     var passed = true;
 
     var durations = new ArrayList<Duration>();
-    durations.add(new Duration(0, 0, 0));
+    durations.add(new Duration(0));
     durations.add(new Duration(0, 0, 1));
-    durations.add(new Duration(0, 1, 0));
+    durations.add(new Duration(60));
     durations.add(new Duration(0, 1, 1));
-    durations.add(new Duration(1, 0, 0));
+    durations.add(new Duration(3600));
     durations.add(new Duration(1, 0, 1));
-    durations.add(new Duration(1, 1, 0));
+    durations.add(new Duration(3660));
     durations.add(new Duration(1, 1, 1));
+    durations.add(new Duration(3662));
 
     for (var i = 0; i < durations.size(); ++i) {
       for (var j = 0; j < durations.size(); ++j) {
@@ -227,8 +179,8 @@ public class Duration implements Comparable<Duration> {
 
         if (indexDiffBound != valueDiffBound) {
           System.err.printf(
-              "Error: Test Duration::testCompareTo failed: Duration{%s}.compareTo(Duration{%s}) == %d\n",
-              di.toString(), dj.toString(), valueDiff);
+              "Error: Test Duration::testCompareTo failed: Duration{%d, %d, %d}.compareTo(Duration{%d, %d, %d}) == %d\n",
+              di._hours, di._minutes, di._seconds, dj._hours, dj._minutes, dj._seconds, valueDiff);
           passed = false;
         }
       }
