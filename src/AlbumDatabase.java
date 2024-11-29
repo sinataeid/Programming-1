fbefd944e7766da289cda4454b93885bf3398263
YK <jqt24wgu@uea.ac.uk>
2024-11-29 18:10:15 +0000

    refactor AlbumDatabase

--- src/AlbumDatabase.java
+++ src/AlbumDatabase.java
@@ -1,55 +1,66 @@
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class AlbumDatabase {
-  public static void main(String[] args) {
-    try {
-      var albumCollection = new AlbumCollection();
-      var albumsFile = new File("./albums.txt");
-      var scanner = new Scanner(albumsFile);
-      var albumDeclarationPattern = Pattern.compile("(^.+) : (.+) \\((\\d+)\\)$", Pattern.MULTILINE);
-      var trackDeclarationPattern = Pattern.compile("^(\\d{1,2}):(\\d{1,2}):(\\d{1,2}) - (.+)$", Pattern.MULTILINE);
+  private static AlbumCollection readAlbumCollectionFile(String fileName) throws FileNotFoundException, UnsupportedOperationException {
+    var result = new AlbumCollection();
 
-        var line = scanner.nextLine();
-      while (scanner.hasNextLine()) {
+    var scanner = new Scanner(new File(fileName));
+    var albumDeclarationPattern = Pattern.compile("^(.+) : (.+) \\((\\d+)\\)$", Pattern.MULTILINE);
+    var trackDeclarationPattern = Pattern.compile("^(\\d{1,2}):(\\d{1,2}):(\\d{1,2}) - (.+)$", Pattern.MULTILINE);
 
-        var albumMatcher = albumDeclarationPattern.matcher(line);
-        if (albumMatcher.find()) {
-            var artist = albumMatcher.group(1);
-            var title = albumMatcher.group(2);
-            var releaseYear = Integer.parseInt(albumMatcher.group(3));
-            var album = new Album(artist, title, releaseYear);
-
-            while (scanner.hasNextLine()) {
-                line = scanner.nextLine();
-
-                var trackMatcher = trackDeclarationPattern.matcher(line);
-                if (trackMatcher.find()) {
-                    title = trackMatcher.group(4);
-                    var hours = Integer.parseInt(trackMatcher.group(1));
-                    var minutes = Integer.parseInt(trackMatcher.group(2));
-                    var seconds = Integer.parseInt(trackMatcher.group(3));
-                    var duration = new Duration(hours, minutes, seconds);
-                    var track = new Track(title, duration);
-                    album.addTrack(track);
-                }
-                else
-                break;
-            }
-
-            albumCollection.addAlbum(album);
-        }
-        else {
-            System.err.printf("Unrecognised line \"%s\". Skipping.\n", line);
-            line = scanner.nextLine();
-        }
+    var line = "";
+    var readNext = true;
+    while (scanner.hasNextLine()) {
+      if (readNext) {
+        line = scanner.nextLine();
       }
 
+      readNext = true;
+
+      var albumMatcher = albumDeclarationPattern.matcher(line);
+      if (!albumMatcher.find()) {
+        throw new UnsupportedOperationException(String.format("Unrecognised line \"%s\"", line));
+      }
+
+      var albumArtist = albumMatcher.group(1);
+      var albumTitle = albumMatcher.group(2);
+      var albumReleaseYear = Integer.parseInt(albumMatcher.group(3));
+      var album = new Album(albumArtist, albumTitle, albumReleaseYear);
+
+      while (scanner.hasNextLine()) {
+        line = scanner.nextLine();
+
+        var trackMatcher = trackDeclarationPattern.matcher(line);
+        if (!trackMatcher.find()) {
+          readNext = false;
+          break;
+        }
+
+        var trackTitle = trackMatcher.group(4);
+        var trackDurationHours = Integer.parseInt(trackMatcher.group(1));
+        var trackDurationMinutes = Integer.parseInt(trackMatcher.group(2));
+        var trackDurationSeconds = Integer.parseInt(trackMatcher.group(3));
+        var trackDuration = new Duration(trackDurationHours, trackDurationMinutes, trackDurationSeconds);
+        var track = new Track(trackTitle, trackDuration);
+        album.addTrack(track);
+      }
+      
+      result.addAlbum(album);
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    try {
+      var albumCollection = readAlbumCollectionFile("./albums.txt");
+
       System.out.println(albumCollection);
-    } catch (IOException e) {
-      System.err.println("An IO exception has occurred.");
+    } catch (Exception e) {
+      System.err.printf("Error: %s\n", e.getMessage());
     }
   }
 }
